1edd4aa1c45682bbca0037fb598735ba2b35249f
Abdur Rahman <81740373+AR007xx@users.noreply.github.com>
2022-02-24 22:09:47 -0800

    Update COVID Portfolio Project - Data Exploration.sql

--- COVID Portfolio Project - Data Exploration.sql
+++ COVID Portfolio Project - Data Exploration.sql
@@ -6,7 +6,8 @@ Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, 
 */
 
 Select *
-From PortfolioProject..CovidDeaths
+From Portfolio
+Project..CovidDeaths
 Where continent is not null 
 order by 3,4
 
